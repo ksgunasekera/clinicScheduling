bee9d1fb0f6e682ae80a5bae4038e349a698088d
ksgunasekera <ksgunasekera@gmail.com>
2017-09-19 00:42:34 +0530

    show message if login inpusts are empty

--- routes/login.js
+++ routes/login.js
@@ -2,12 +2,16 @@ const express=require('express');
 const router=express.Router();
 
 router.get('/',(request,respond)=>{
-	respond.render('login');
+	respond.render('login',{error:false,message:""});
 });
 router.post('/login',(request,respond)=>{
 	const data=request.body;
 	console.log(data.username);
 	console.log(data.password);
+	if((data.username==""&&data.password=="")||data.username===""||data.password===""){
+		respond.render('login',{error:true,message:"Invalid login"});
+		respond.redirect('/')
+	}
 });
 
 module.exports =router;
